b0c2c64dedd4d51d53c76574974287ff34831532
Bilal Almazbekov <138653003+bilyashik@users.noreply.github.com>
2023-12-04 18:11:06 +0600

    Add files via upload

---
+++ homework-1/app.js
@@ -0,0 +1,42 @@
+// задание 1
+
+// числа
+let numericVariable = 42;
+
+// стринг
+let stringVariable = "Hello, World!";
+
+// boolean
+let booleanVariable = true;
+
+// undefined
+let undefinedVariable;
+
+// object
+let workingHours = {
+    start: '09:00',
+    end: '18:00'
+};
+
+// задание 2
+
+let bankBranch = {
+    address: "Юнусалиева",
+    isMainBranch: true,
+    numberOfEmployees: 34,
+    workingHours: {
+        start: '09:00',
+        end: '18:00'
+    }
+};
+
+
+// задание 3
+
+let studentInformation = {
+    isGraduete: true,
+    nameOfStudent: "Билал",
+    surenameOfStudent: "Алмазбеков",
+    yearsOld: 15,
+    group: "33-01"
+}
